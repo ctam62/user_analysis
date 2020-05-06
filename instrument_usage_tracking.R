8133309fdfd7a56a4f872712454dc4e5c30cd279
claratam62 <claratam62@gmail.com>
2020-05-06 00:14:04 -0500

    used plyr package to run count and aggregate functions; added dplyr package

--- instrument_usage_tracking.R
+++ instrument_usage_tracking.R
@@ -6,18 +6,30 @@ Microscopy instrument usage tracking
 # Set working directory
 setwd("D:/instrument_usage_tracking")
 
+# import required library packages
+library(plyr)
+library(dplyr)
+
 #---------------------------------------
 # Define the input arguments
 #---------------------------------------
 # Microscopy Fees (Update as necessary)
-data_analysis = 0
-user = 25
-training = 100
+price_list <- c(data_analysis=0, user=10, training=50)
 
 #---------------------------------------
 
 # import excel file
-DATA <- read.csv(file='2019-8 Zeiss_Epi', header=TRUE)
+data_df <- read.csv(file='2019-8 Zeiss_Epi.csv', header=TRUE, stringsAsFactors=FALSE)
+supervisors <- data_df$Supervisor
+price <- data_df$Price
+instrument <- data_df$ï..Zeiss.Epi
 
+num_supervisors <- count(data_df, c("Supervisor"))
+usage_report <- aggregate(Price ~ Supervisor + ï..Zeiss.Epi, data=data_df, sum)
+
+# usage_report <- instrument_usage(price_list){
+
+
+# }
 
 
