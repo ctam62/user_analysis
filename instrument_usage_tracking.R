0090d030ccb3115bc9a5ad799b478aa0335f3170
claratam62 <claratam62@gmail.com>
2020-05-05 20:25:45 -0500

    added argument statements; imported excel file

--- instrument_usage_tracking.R
+++ instrument_usage_tracking.R
@@ -1,9 +1,23 @@
 "
-Microscopy instrument usage tracking function
+Microscopy instrument usage tracking
 
 "
 
-#Set working directory
+# Set working directory
 setwd("D:/instrument_usage_tracking")
 
-#Microscopy Fees# Update as necessary
+#---------------------------------------
+# Define the input arguments
+#---------------------------------------
+# Microscopy Fees (Update as necessary)
+data_analysis = 0
+user = 25
+training = 100
+
+#---------------------------------------
+
+# import excel file
+DATA <- read.csv(file='2019-8 Zeiss_Epi', header=TRUE)
+
+
+
