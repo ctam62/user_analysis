48adc61ceeaa64113254e891ed6ddfd9a6539581
claratam62 <claratam62@gmail.com>
2020-06-06 10:22:58 -0500

    created utils R package; moved helper functions into the utils R package; imported utils module into the main script

--- monthly_instrument_usage_tracking.R
+++ monthly_instrument_usage_tracking.R
@@ -9,31 +9,31 @@ Licensed under the MIT License (see LICENSE for details)
 #--------------------------------------------------------------------------------
 # Import required library packages
 #--------------------------------------------------------------------------------
-library(rstudioapi)
-library(plyr)
+import::from(rstudioapi, getActiveDocumentContext)
+import::from(data.table, fread)
+import::from(lubridate, hm)
 library(stringr)
-library(openxlsx)
-library(data.table)
-library(lubridate)
+library(modules)
 
 #--------------------------------------------------------------------------------
 # Setup working environment
 #--------------------------------------------------------------------------------
 # Set working directory
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd(dirname(getActiveDocumentContext()$path))
 working_dir <- getwd()
 cat("Your working directory is set to:", working_dir, "\n")
 
 #--------------------------------------------------------------------------------
 # Import files
 #--------------------------------------------------------------------------------
-# batch import csv files
+# batch import csv files from data directory
 csv_files <- Sys.glob(paste0(working_dir,'/data/*.csv'))
-csv_file_names <- gsub(".*/", "", csv_files)
+csv_filenames <- gsub(".*/", "", csv_files)
 
 #--------------------------------------------------------------------------------
 # Instrument Fees (Update as necessary)
 #--------------------------------------------------------------------------------
+# define the price list for each instrument
 instrumentA_price_list <- c(
   "Training" = 75, 
   "User" = 50, 
@@ -64,200 +64,42 @@ instrumentC_price_list <- c(
   "External Full Service" = 75
   )
 
+# store instrument specific price lists in price_list
 price_list <- list("Instrument_A" = instrumentA_price_list, 
                    "Instrument_B" = instrumentB_price_list,
                    "Instrument_C" = instrumentC_price_list)
 
 #--------------------------------------------------------------------------------
-# Functions
-#--------------------------------------------------------------------------------
-dataframe_preprocessing <- function(data_df){
-  "
-  Arguments:
-  data_df -- dataframe, of shape (num_obs, num_variables)
-  
-  Returns:
-  data_df -- dataframe, of shape (num_obs, num_variables)
-  "
-  # remove nonalphanumeric characters from header names
-  names(data_df) <- gsub("[^[:alnum:]///' ]", "", names(data_df))
-  # names(data_df)[1] <- substring(names(data_df)[1],2)
-  # standardize supervisor names by removing any punctuations
-  data_df$Supervisor <- gsub("[[:punct:]]", "", data_df$Supervisor)
-  
-  return(data_df)
-}
-
-
-compute_timespent <-function(data_df){
-  "
-  Arguments:
-  data_df -- dataframe, of shape (num_obs, num_variables)
-  
-  Return:
-  num_hours -- numeric value
-  
-  "
-  # reformat Starttime and Finishtime
-  data_df$Starttime <- sub(".+? ", "", data_df$Starttime)
-  data_df$Finishtime <- sub(".+? ", "", data_df$Finishtime)
-  # extract time values
-  starttime <- as.numeric(hm(data_df$Starttime), units="hours")
-  endtime <- as.numeric(hm(data_df$Finishtime), units="hours")
-  # compute the difference
-  num_hours <- abs(endtime - starttime)
-  
-  return(num_hours)
-}
-
-
-instrument_usage <- function(data_df, price_list){
-  " 
-  Arguments:
-  data_df -- input dataframe, of shape (num_obs, num_variables)
-  price_list -- named atomic vector type numeric, of shape (1, 3)
-  
-  Return:
-  usage_report -- dataframe, of shape (num_obs, 4)
-  payment_report -- dataframe, of shape (num_obs, 2)
-  "
-  
-  if(names(data_df)[1] == "InstrumentA"){
-    # Confocal instrument usage report
-    df_first_column <- data_df$InstrumentA
-    
-    # calculate the number of hours spent per task
-    # preallocate usage_hours vector
-    usage_hours <- c()
-    for (item in 1:length(price_list)){
-      if (names(price_list[item]) %in% df_first_column){
-        # extract data for Instrument
-        temp <- subset(data_df, InstrumentA == names(price_list)[item])
-        num_hours <- compute_timespent(temp)
-        usage_hours <- append(usage_hours, num_hours, after=length(usage_hours))
-      }# end if
-    }# end for
-    
-    usage_formula <- cbind(Price, Hours) ~ Supervisor + InstrumentA
-    user_formula <- cbind(Price, Hours) ~ Supervisor + Fullname + InstrumentA
-    report_col_names <- c("Supervisor", "Fullname", "InstrumentA", "Price")
-  
-  }else if (names(data_df)[1] == "InstrumentB"){
-    # EpiCalcium instrument usage report  
-    df_first_column <- data_df$InstrumentB
-    
-    # calculate the number of hours spent per task
-    # preallocate usage_hours vector
-    usage_hours <- c()
-    for (item in 1:length(price_list)){
-      if (names(price_list[item]) %in% df_first_column){
-        # extract data for Instrument
-        temp <- subset(data_df, InstrumentB == names(price_list)[item])
-        num_hours <- compute_timespent(temp)
-        usage_hours <- append(usage_hours, num_hours, after=length(usage_hours))
-      }# end if
-    }# end for
-    
-    usage_formula <- cbind(Price, Hours) ~ Supervisor + InstrumentB
-    user_formula <- cbind(Price, Hours) ~ Supervisor + Fullname + InstrumentB
-    report_col_names <- c("Supervisor", "Fullname", "InstrumentB", "Price")
-    
-  }else if(names(data_df)[1] == "InstrumentC"){
-    # ZeissEpi instrument usage report
-    df_first_column <- data_df$InstrumentC
-    
-    # calculate the number of hours spent per task
-    # preallocate usage_hours vector
-    usage_hours <- c()
-    for (item in 1:length(price_list)){
-      if (names(price_list[item]) %in% df_first_column){
-        # extract data for Instrument
-        temp <- subset(data_df, InstrumentC == names(price_list)[item])
-        num_hours <- compute_timespent(temp)
-        usage_hours <- append(usage_hours, num_hours, after=length(usage_hours))
-      }# end if
-    }# end for
-    
-    usage_formula <- cbind(Price, Hours) ~ Supervisor + InstrumentC
-    user_formula <- cbind(Price, Hours) ~ Supervisor + Fullname + InstrumentC
-    report_col_names <- c("Supervisor", "Fullname", "InstrumentC", "Price")
-  }
-  
-  # extract supversior and instrument columns  
-  temp_report <- subset(data_df, select=report_col_names)
-    
-  # create usage_report dataframe with hours
-  temp_report$Hours <- usage_hours
-  usage_report <- aggregate(usage_formula, data=temp_report, sum)
-    
-  # create user_report
-  user_report <- aggregate(user_formula, data=temp_report, sum)
-    
-  # calculate total price for each supervisor
-  payment_report <- aggregate(Price ~ Supervisor, data=data_df, sum)
-  
-  # create list to return multiple reports
-  return_list <- list(usage_report, user_report, payment_report)
-
-  return(return_list)
-}
-
-
-export_reports <- function(reports, savename){
-  "
-  Arguments:
-  reports -- list, of shape (2, 1)
-  savename -- string, of shape (1, 1)
-  "
-  # convert individual reports back to dataframes
-  usage_report <- data.frame(reports[1], stringsAsFactors=FALSE)
-  usage_report <- usage_report[order(usage_report[,1]),] # sort df by supervisor
-  user_report <- data.frame(reports[2], stringsAsFactors=FALSE)
-  user_report <- user_report[order(user_report[,1]),] # sort df by supervisor
-  payment_report <- data.frame(reports[3], stringsAsFactors=FALSE)
-  
-  # export reports
-  wb <- createWorkbook()
-  addWorksheet(wb, "Instrument Usage Report")
-  addWorksheet(wb, "User Report")
-  addWorksheet(wb, "Payment Totals")
-  
-  writeData(wb, "Instrument Usage Report", usage_report)
-  writeData(wb, "User Report", user_report)
-  writeData(wb, "Payment Totals", payment_report)
-  
-  saveWorkbook(wb, file=savename, overwrite=TRUE)
-  cat("\nFile:", savename, " has been created\n")
-}
-
-#--------------------------------------------------------------------------------
 # Main Script
 #--------------------------------------------------------------------------------
+# import helper functions from utils module
+utils <- use("utils.R")
+
 # batch read csv files
 csv_data = lapply(csv_files, fread)
 
-# obtain dates and instrument names
-dates <- gsub("[A-z.&/: ]", "", csv_file_names)
-instruments <- gsub("[0-9 -]|+.csv", "", csv_file_names)
+# obtain dates and instrument names from csv filenames
+dates <- gsub("[A-z.&/: ]", "", csv_filenames)
+instruments <- gsub("[0-9 -]|+.csv", "", csv_filenames)
 
 for(item in 1:length(csv_files)){
-  # batch generate output directory and filenames
+  # generate output directory and filenames
   cat("Generating reports for", instruments[item],"....")
   output_dir <- file.path(working_dir, paste0(dates[item],"_reports"))
   if (!dir.exists(output_dir)){dir.create(output_dir)}
   savename <- paste0(output_dir, "/", dates[item], "_", instruments[item],"_usage_report.xlsx")
   
-  # batch process and generate reports
+  # generate reports
   imported_df <- data.frame(csv_data[item], stringsAsFactors=FALSE)
-  preprocessed_df <- dataframe_preprocessing(imported_df)
-  # check if dataframe is empty
+  preprocessed_df <- utils$dataframe_preprocessing(imported_df)
+  # check if data frame is empty
   if (empty(preprocessed_df)){cat("\n", names(preprocessed_df)[1], 
                               " has no data. Usage report was not created.\n")
   } else{
     # retrieve reports from instrument_usage function
     index <- match(instruments[item], names(price_list))
-    instrument_reports <- instrument_usage(preprocessed_df, price_list[[index]])
-    # export report
-    export_reports(instrument_reports, savename)
+    instrument_reports <- utils$instrument_usage(preprocessed_df, price_list[[index]])
+    # export reports
+    utils$export_reports(instrument_reports, savename)
   }# end else
 }# end for
---
+++ utils.R
@@ -0,0 +1,170 @@
+import("stats", "aggregate")
+import("openxlsx")
+import("lubridate", "hm")
+
+
+dataframe_preprocessing <- function(data_df){
+  "
+  Remove unwanted columns and whitespaces in column header names
+  
+  Arguments:
+  data_df -- data frame, of shape (num_obs, num_variables)
+  
+  Returns:
+  data_df -- data frame, of shape (num_obs, num_variables)
+  "
+  # remove nonalphanumeric characters from header names
+  names(data_df) <- gsub("[^[:alnum:]///' ]", "", names(data_df))
+  # standardize supervisor names by removing any punctuations
+  data_df$Supervisor <- gsub("[[:punct:]]", "", data_df$Supervisor)
+  
+  return(data_df)
+}
+
+compute_timespent <-function(data_df){
+  "
+  Comput the number of hours spent per user
+  
+  Arguments:
+  data_df -- data frame, of shape (num_obs, num_variables)
+  
+  Return:
+  num_hours -- numeric value
+  
+  "
+  # reformat Starttime and Finishtime
+  data_df$Starttime <- sub(".+? ", "", data_df$Starttime)
+  data_df$Finishtime <- sub(".+? ", "", data_df$Finishtime)
+  # extract time values
+  starttime <- as.numeric(hm(data_df$Starttime), units="hours")
+  endtime <- as.numeric(hm(data_df$Finishtime), units="hours")
+  # compute the difference
+  num_hours <- abs(endtime - starttime)
+  
+  return(num_hours)
+}
+
+
+instrument_usage <- function(data_df, price_list){
+  " 
+  Compiles usage, user, and payment reports
+  
+  Arguments:
+  data_df -- input data frame, of shape (num_obs, num_variables)
+  price_list -- named atomic vector type numeric, of shape (1, 3)
+  
+  Return:
+  usage_report -- data frame, of shape (num_obs, 4)
+  payment_report -- data frame, of shape (num_obs, 2)
+  "
+  
+  if(names(data_df)[1] == "InstrumentA"){
+    # Confocal instrument usage report
+    df_first_column <- data_df$InstrumentA
+    
+    # calculate the number of hours spent per task
+    # preallocate usage_hours vector
+    usage_hours <- c()
+    for (item in 1:length(price_list)){
+      if (names(price_list[item]) %in% df_first_column){
+        # extract data for Instrument
+        temp <- subset(data_df, InstrumentA == names(price_list)[item])
+        num_hours <- compute_timespent(temp)
+        usage_hours <- append(usage_hours, num_hours, after=length(usage_hours))
+      }# end if
+    }# end for
+    
+    usage_formula <- cbind(Price, Hours) ~ Supervisor + InstrumentA
+    user_formula <- cbind(Price, Hours) ~ Supervisor + Fullname + InstrumentA
+    report_col_names <- c("Supervisor", "Fullname", "InstrumentA", "Price")
+    
+  }else if (names(data_df)[1] == "InstrumentB"){
+    # EpiCalcium instrument usage report  
+    df_first_column <- data_df$InstrumentB
+    
+    # calculate the number of hours spent per task
+    # preallocate usage_hours vector
+    usage_hours <- c()
+    for (item in 1:length(price_list)){
+      if (names(price_list[item]) %in% df_first_column){
+        # extract data for Instrument
+        temp <- subset(data_df, InstrumentB == names(price_list)[item])
+        num_hours <- compute_timespent(temp)
+        usage_hours <- append(usage_hours, num_hours, after=length(usage_hours))
+      }# end if
+    }# end for
+    
+    usage_formula <- cbind(Price, Hours) ~ Supervisor + InstrumentB
+    user_formula <- cbind(Price, Hours) ~ Supervisor + Fullname + InstrumentB
+    report_col_names <- c("Supervisor", "Fullname", "InstrumentB", "Price")
+    
+  }else if(names(data_df)[1] == "InstrumentC"){
+    # ZeissEpi instrument usage report
+    df_first_column <- data_df$InstrumentC
+    
+    # calculate the number of hours spent per task
+    # preallocate usage_hours vector
+    usage_hours <- c()
+    for (item in 1:length(price_list)){
+      if (names(price_list[item]) %in% df_first_column){
+        # extract data for Instrument
+        temp <- subset(data_df, InstrumentC == names(price_list)[item])
+        num_hours <- compute_timespent(temp)
+        usage_hours <- append(usage_hours, num_hours, after=length(usage_hours))
+      }# end if
+    }# end for
+    
+    usage_formula <- cbind(Price, Hours) ~ Supervisor + InstrumentC
+    user_formula <- cbind(Price, Hours) ~ Supervisor + Fullname + InstrumentC
+    report_col_names <- c("Supervisor", "Fullname", "InstrumentC", "Price")
+  }
+  
+  # extract supversior and instrument columns  
+  temp_report <- subset(data_df, select=report_col_names)
+  
+  # create usage_report data frame with hours
+  temp_report$Hours <- usage_hours
+  usage_report <- aggregate(usage_formula, data=temp_report, sum)
+  
+  # create user_report
+  user_report <- aggregate(user_formula, data=temp_report, sum)
+  
+  # calculate total price for each supervisor
+  payment_report <- aggregate(Price ~ Supervisor, data=data_df, sum)
+  
+  # create list to return multiple reports
+  return_list <- list(usage_report, user_report, payment_report)
+  
+  return(return_list)
+}
+
+export_reports <- function(reports, savename){
+  "
+  Export reports to an excel workbook
+  
+  Arguments:
+  reports -- list, of shape (2, 1)
+  savename -- string, of shape (1, 1)
+  "
+  # convert individual reports back to data frames
+  usage_report <- data.frame(reports[1], stringsAsFactors=FALSE)
+  usage_report <- usage_report[order(usage_report[,1]),] # sort df by supervisor
+  user_report <- data.frame(reports[2], stringsAsFactors=FALSE)
+  user_report <- user_report[order(user_report[,1]),] # sort df by supervisor
+  payment_report <- data.frame(reports[3], stringsAsFactors=FALSE)
+  
+  # create a workbook object and add worksheets
+  wb <- createWorkbook()
+  addWorksheet(wb, "Instrument Usage Report")
+  addWorksheet(wb, "User Report")
+  addWorksheet(wb, "Payment Totals")
+  
+  # write data into the workbook object
+  writeData(wb, "Instrument Usage Report", usage_report)
+  writeData(wb, "User Report", user_report)
+  writeData(wb, "Payment Totals", payment_report)
+  
+  # save the workbook
+  saveWorkbook(wb, file=savename, overwrite=TRUE)
+  cat("\nFile:", savename, " has been created\n")
+}
